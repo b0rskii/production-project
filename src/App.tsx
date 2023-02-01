6330e8933d9a00a4a3313e62a65e247fde737f8c
b0rskii <gldimon@mail.ru>
2023-02-01 15:59:19 +0700

    Добавление роутинга с lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { Test } from './components/test';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Suspense } from 'react';
+import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
+import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
 import './index.scss';
 
-export function App() {
+function App() {
   return (
     <div className='app'>
-      <Test />
+      <Link to='/'>Main</Link>
+      <Link to='/about'>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<MainPageLazy />} />
+          <Route path='/about' element={<AboutPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
+
+export default App;
--- src/components/Test.tsx
+++
@@ -1,9 +0,0 @@
-import style from './Test.module.scss';
-
-export function Test() {
-  return (
-    <div className={style.test}>
-      Work!
-    </div>
-  );
-}
