c2d46b7f3f3b20e315286209aebb86d7bdb2f508
b0rskii <gldimon@mail.ru>
2023-02-02 12:28:06 +0700

    Организация стилей. Добавление темы

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,26 @@ import { Routes, Route, Link } from 'react-router-dom';
 import { Suspense } from 'react';
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
-import './index.scss';
+import { useTheme } from './theme/useTheme';
+import './styles/index.scss';
+
+enum AppRoute {
+  MAIN = '/',
+  ABOUT = '/about',
+}
 
 function App() {
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <div className='app'>
-      <Link to='/'>Main</Link>
-      <Link to='/about'>About</Link>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>ToggleTheme</button>
+      <Link to={AppRoute.MAIN}>Main</Link>
+      <Link to={AppRoute.ABOUT}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path='/' element={<MainPageLazy />} />
-          <Route path='/about' element={<AboutPageLazy />} />
+          <Route path={AppRoute.MAIN} element={<MainPageLazy />} />
+          <Route path={AppRoute.ABOUT} element={<AboutPageLazy />} />
         </Routes>
       </Suspense>
     </div>
