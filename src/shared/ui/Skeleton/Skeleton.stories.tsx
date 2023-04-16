fa511ed2e1da5f4c21d728c7ff7b5e0162bb98ac
b0rskii <gldimon@mail.ru>
2023-04-16 17:50:50 +0700

    feat: добавляет стори на Skeleton

---
+++ src/shared/ui/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,41 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
+import { Theme } from 'shared/utils/theme';
+import { Skeleton } from './Skeleton';
+
+export default {
+  title: 'shared/Skeleton',
+  component: Skeleton,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  args: {
+    height: 200,
+  },
+} as ComponentMeta<typeof Skeleton>;
+
+const Template: ComponentStory<typeof Skeleton> = (args) => <Skeleton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  width: '100%',
+};
+
+export const Round = Template.bind({});
+Round.args = {
+  width: 200,
+  borderRadius: '50%',
+};
+
+export const DefaultDark = Template.bind({});
+DefaultDark.args = {
+  width: '100%',
+};
+DefaultDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const RoundDark = Template.bind({});
+RoundDark.args = {
+  width: 200,
+  borderRadius: '50%',
+};
+RoundDark.decorators = [ThemeDecorator(Theme.DARK)];
