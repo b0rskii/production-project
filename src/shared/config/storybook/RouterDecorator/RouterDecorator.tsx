1c46388b619434bc826c67906c497b2420c44f38
b0rskii <gldimon@mail.ru>
2023-04-16 18:12:31 +0700

    fix: добавляет Suspense в RouterDecorator

--- src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
+++ src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
@@ -1,8 +1,11 @@
 import { Story } from '@storybook/react';
+import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 export const RouterDecorator = (story: () => Story) => (
   <BrowserRouter>
-    {story()}
+    <Suspense fallback="">
+      {story()}
+    </Suspense>
   </BrowserRouter>
 );
