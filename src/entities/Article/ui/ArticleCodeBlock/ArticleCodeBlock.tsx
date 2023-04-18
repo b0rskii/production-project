ffc214685f3f8c336b39fecfa08d843f9228d277
b0rskii <gldimon@mail.ru>
2023-04-18 14:32:25 +0700

    fix: убирает пустые стили

--- src/entities/Article/ui/ArticleCodeBlock/ArticleCodeBlock.tsx
+++ src/entities/Article/ui/ArticleCodeBlock/ArticleCodeBlock.tsx
@@ -2,7 +2,6 @@ import { PropsWithChildren, memo } from 'react';
 import { getClassNames } from 'shared/utils/classNames';
 import { Code } from 'shared/ui/Code';
 import { ArticleCodeBlock as IArticleCodeBlock } from '../../model/types/articleSchema';
-import style from './ArticleCodeBlock.module.scss';
 
 type ArticleCodeBlockProps = PropsWithChildren<{
   className?: string;
@@ -13,7 +12,7 @@ export const ArticleCodeBlock = memo((props: ArticleCodeBlockProps) => {
   const { className, content } = props;
 
   return (
-    <div className={getClassNames(style.articleCodeBlock, {}, [className])}>
+    <div className={getClassNames('', {}, [className])}>
       <Code code={content.code} />
     </div>
   );
