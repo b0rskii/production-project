d6c1c3e301b280a1743b5cea7feb2b410bed88b7
b0rskii <gldimon@mail.ru>
2023-04-26 12:33:19 +0700

    feat: делает ссылкой на профиль имя и аватар комментария статьи

--- src/widgets/CommentCard/ui/CommentCard/CommentCard.tsx
+++ src/widgets/CommentCard/ui/CommentCard/CommentCard.tsx
@@ -2,7 +2,9 @@ import { PropsWithChildren, memo } from 'react';
 import { getClassNames } from 'shared/utils/classNames';
 import { Avatar } from 'shared/ui/Avatar';
 import { Text } from 'shared/ui/Text';
+import { AppLink } from 'shared/ui/AppLink';
 import { Comment } from 'shared/types/comment';
+import { RoutePath } from 'shared/config/routing';
 import style from './CommentCard.module.scss';
 
 type CommentCardProps = PropsWithChildren<{
@@ -16,12 +18,12 @@ export const CommentCard = memo((props: CommentCardProps) => {
 
   return (
     <div className={getClassNames(style.commentCard, {}, [className])}>
-      <div className={style.header}>
+      <AppLink className={style.header} to={`${RoutePath.PROFILES}${user.id}`}>
         {user.avatar && (
           <Avatar src={user.avatar} alt={user.username} size={30} />
         )}
         <Text title={user.username} />
-      </div>
+      </AppLink>
       <Text text={text} />
     </div>
   );
