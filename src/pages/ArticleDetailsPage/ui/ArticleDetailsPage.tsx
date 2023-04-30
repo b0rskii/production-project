c22ce7aef86655da0f546f2a41121ce4e4fcc52e
b0rskii <gldimon@mail.ru>
2023-04-30 22:07:35 +0700

    feat: добавляет на страницу статьи кнопку к списку статей

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage.tsx
@@ -1,6 +1,10 @@
-import { PropsWithChildren, memo } from 'react';
-import { useParams } from 'react-router-dom';
+import { PropsWithChildren, memo, useCallback } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { I18nNameSpace } from 'shared/utils/i18n/nameSpace';
 import { getClassNames } from 'shared/utils/classNames';
+import { RoutePath } from 'shared/config/routing';
+import { Button, ButtonTheme } from 'shared/ui/Button';
 import { ArticleDetailsBlock } from './ArticleDetailsBlock';
 import { ArticleCommentsBlock } from './ArticleCommentsBlock';
 import style from './ArticleDetailsPage.module.scss';
@@ -12,9 +16,21 @@ type ArticleDetailsPageProps = PropsWithChildren<{
 const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
   const { className } = props;
   const { id } = useParams();
+  const { t } = useTranslation(I18nNameSpace.Article);
+  const navigate = useNavigate();
+
+  const toArticlesListButtonHandler = useCallback(() => {
+    navigate(RoutePath.ARTICLES);
+  }, [navigate]);
 
   return (
     <div className={getClassNames('', {}, [className])}>
+      <Button
+        theme={ButtonTheme.OUTLINE}
+        onClick={toArticlesListButtonHandler}
+      >
+        {t('К списку статей')}
+      </Button>
       <ArticleDetailsBlock articleId={id} />
       <ArticleCommentsBlock className={style.block} articleId={id} />
     </div>
