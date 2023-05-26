6b44d9c57628dbc8c6648086f8f384461d3605fb
b0rskii <gldimon@mail.ru>
2023-05-26 11:47:50 +0700

    feat: добавляет стори для компонентов уведомлений

--- src/shared/utils/notifications/ui/NotificationsList/NotificationsList.stories.tsx
+++ src/shared/utils/notifications/ui/NotificationsList/NotificationsList.stories.tsx
@@ -2,9 +2,13 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
 import { Theme } from 'shared/utils/theme';
 import { NotificationsList } from './NotificationsList';
+import { NotificationItem } from '../Notification';
+
+const notification: NotificationItem = { text: 'Операция прошла успешно', type: 'success' };
+const notifications = [notification, notification, notification];
 
 export default {
-  title: 'entities/NotificationsList',
+  title: 'shared/NotificationsList',
   component: NotificationsList,
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -14,8 +18,23 @@ export default {
 const Template: ComponentStory<typeof NotificationsList> = (args) => <NotificationsList {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  notifications,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+  notifications: [],
+};
 
 export const DefaultDark = Template.bind({});
-DefaultDark.args = {};
+DefaultDark.args = {
+  notifications,
+};
 DefaultDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const EmptyDark = Template.bind({});
+EmptyDark.args = {
+  notifications: [],
+};
+EmptyDark.decorators = [ThemeDecorator(Theme.DARK)];
