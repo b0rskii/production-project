9a24efe1637068089ed07f5ec65777bb52046d1f
b0rskii <gldimon@mail.ru>
2023-06-29 22:05:02 +0700

    feat: добавляет сообщение при пустом списке статей

--- src/2_pages/ArticlesPage/ui/ArticlesBlock/ArticlesBlock.tsx
+++ src/2_pages/ArticlesPage/ui/ArticlesBlock/ArticlesBlock.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import {
   ARTICLES_LIMIT,
   ARTICLES_SLICE,
@@ -12,7 +13,9 @@ import {
 import { getClassNames } from '6_shared/utils/classNames';
 import { useAppDispatch, useAsyncReducer } from '6_shared/utils/redux';
 import { LocalStorageKey } from '6_shared/const/localStorage';
+import { I18nNameSpace } from '6_shared/utils/i18n/nameSpace';
 import { ListView } from '6_shared/ui/ListViewSwitcher';
+import { Text } from '6_shared/ui/Text';
 import style from './ArticlesBlock.module.scss';
 
 let isInit = true;
@@ -23,6 +26,7 @@ type ArticlesBlockProps = PropsWithChildren<{
 
 export const ArticlesBlock = (props: ArticlesBlockProps) => {
   const { className } = props;
+  const { t } = useTranslation(I18nNameSpace.Article);
   const dispatch = useAppDispatch();
 
   useAsyncReducer(ARTICLES_SLICE, articlesReducer, false);
@@ -47,6 +51,14 @@ export const ArticlesBlock = (props: ArticlesBlockProps) => {
       }
     }
   }, [dispatch]);
+
+  if (!isLoading && !error && !articles.length) {
+    return (
+      <section className={getClassNames(style.articlesBlock, {}, [className])}>
+        <Text title={t('Статьи не найдены')} align="center" />
+      </section>
+    );
+  }
 
   return (
     <section className={getClassNames(style.articlesBlock, {}, [className])}>
