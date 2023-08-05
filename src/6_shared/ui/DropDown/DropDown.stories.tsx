14699309bba37307cd320a21bbb41b98a6385704
b0rskii <gldimon@mail.ru>
2023-08-05 15:00:29 +0700

    service: добавляет стори на компонент DropDown

--- src/6_shared/ui/DropDown/DropDown.stories.tsx
+++ src/6_shared/ui/DropDown/DropDown.stories.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from '6_shared/config/storybook/ThemeDecorator';
 import { Theme } from '6_shared/utils/theme';
+import { Avatar } from '6_shared/ui/Avatar';
 import { DropDown } from './DropDown';
+
+const AVATAR_URL = 'https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg';
 
 export default {
   title: '6_shared/DropDown',
@@ -9,14 +13,42 @@ export default {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
+  args: {
+    button: (
+      <Avatar
+        src={AVATAR_URL}
+        alt="avatar"
+        size={35}
+      />
+    ),
+    items: [
+      { content: 'Профиль' },
+      { content: 'Премиум', disabled: true },
+      { content: 'Выйти' },
+    ],
+  },
 } as ComponentMeta<typeof DropDown>;
 
-// eslint-disable-next-line react/jsx-props-no-spreading
-const Template: ComponentStory<typeof DropDown> = (args) => <DropDown {...args} />;
+const Template: ComponentStory<typeof DropDown> = (args) => (
+  <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <DropDown {...args} />
+  </div>
+);
 
 export const Default = Template.bind({});
 Default.args = {};
 
+export const LeftDirection = Template.bind({});
+LeftDirection.args = {
+  direction: 'left',
+};
+
 export const DefaultDark = Template.bind({});
 DefaultDark.args = {};
 DefaultDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const LeftDirectionDark = Template.bind({});
+LeftDirectionDark.args = {
+  direction: 'left',
+};
+LeftDirectionDark.decorators = [ThemeDecorator(Theme.DARK)];
