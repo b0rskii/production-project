74d09e47e83f0e04da74fb811544a1dd6310484b
b0rskii <gldimon@mail.ru>
2025-03-28 21:04:58 +0700

    fix: исправляет баг в GlobalCursor

--- src/6_shared/utils/globalCursor.ts
+++ src/6_shared/utils/globalCursor.ts
@@ -4,9 +4,12 @@ class GlobalCursor {
   private styleElement: HTMLStyleElement | null = null;
 
   set(cursor: string) {
+    if (this.styleElement) return;
+
     this.styleElement = document.createElement('style');
     this.styleElement.id = STYLE_ELEMENT_ID;
     this.styleElement.textContent = `* {cursor: ${cursor} !important}`;
+
     document.documentElement.prepend(this.styleElement);
   }
 
